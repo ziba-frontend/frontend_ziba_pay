9b0ca8555cac2570954052ff5276d7071762b1b9
bertin <iradukundabertin082@gmail.com>
2024-07-01 15:33:44 +0200

    modify success page

--- src/app/(checkout)/(without-sidebar)/payment-success/page.tsx
+++ src/app/(checkout)/(without-sidebar)/payment-success/page.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import successIm from "../../../../public/images/success.png"
-import Image from "next/image";
+import { LockKeyhole } from "lucide-react";
+import successIm from "../../../../../public/images/success.png";
 import Link from "next/link";
-const page = () => {
-  return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-    <Image src={successIm} alt="success"/>
-    <p className="text-2xl">Payment Successful</p>
-    <small className="text-gray-500">You paid RWF 350 to Kigali Mart</small>
-    <Link href="/">Go Back to Homepage</Link>
-</div>
-  )
-}
+import React from "react";
+import Image from "next/image";
 
-export default page
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+   return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+         <div className=" mx-auto w-full md:w-5/6 lg:w-[50%]"> <div className="flex flex-col gap-4 items-center justify-center">
+         <Image
+            src={successIm}
+            alt="success"
+         />
+         <p className="text-2xl">Payment Successful</p>
+         <small className="text-gray-500">
+            You paid RWF 350 to Kigali Mart
+         </small>
+         <Link href="/">Go Back to Homepage</Link>
+      </div></div>
+         <div className="flex gap-2 mt-12">
+            <LockKeyhole />{" "}
+            <p>
+               Secured by{" "}
+               <Link
+                  href="/"
+                  className="font-bold"
+               >
+                  ZibaPay
+               </Link>
+            </p>
+         </div>
+      </div>
+   );
+};
+
+export default Layout;
