838a87e0de066a84895da5e881f28e2935eabb1b
bertin <iradukundabertin082@gmail.com>
2024-08-20 20:33:35 +0200

    modify checkout

--- src/app/(checkout)/(with-sidebar)/layout.tsx
+++ src/app/(checkout)/(with-sidebar)/layout.tsx
@@ -4,14 +4,23 @@ import CheckoutSidebar from "@/components/CheckoutSidebar";
 import { LockKeyhole } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: "Checkout - Ziba Pay",
+   description: "Zibapay checkout page",
+};
+
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+   
+
    return (
-      <div className="flex flex-col items-center justify-center min-h-screen  py-12">
-         <div className=" mx-auto w-full md:w-5/6 lg:w-[50%]">
+      <div className="flex flex-col items-center justify-center min-h-screen py-12">
+         <div className="mx-auto w-full md:w-5/6 xl:w-[50%]">
             <div className="flex shadow-lg border">
                <CheckoutSidebar />
-               <section className="transition-all duration-300 py-6 pr-4 md:pr-10    w-full">
+               <section className="transition-all duration-300 py-6 pr-4 md:pr-10 w-full">
                   <CheckoutNavbar />
                   {children}
                   <CheckoutFooter />
@@ -22,14 +31,12 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <LockKeyhole />{" "}
             <p>
                Secured by{" "}
-               <Link
-                  href="/"
-                  className="font-bold"
-               >
+               <Link href="/" className="font-bold">
                   ZibaPay
                </Link>
             </p>
          </div>
+        
       </div>
    );
 };
--- src/components/CheckoutSidebar.tsx
+++ src/components/CheckoutSidebar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import im1 from "../../public/images/mobile_checkout.svg";
 const CheckoutSidebar = () => {
    return (
-      <div className=" bg-[#F4F4F4] w-[100px] md:w-[280px] border-r p-4 transition-width duration-300 space-y-10 text-start mr-4">
+      <div className=" bg-[#F4F4F4] hidden sm:block w-[100px] md:w-[280px] border-r p-4 transition-width duration-300 space-y-10 text-start mr-4">
          <h1 className="text-sm md:text-2xl text-[#4C4C4C] font-semibold  ">PAY WITH</h1>
          <div className="flex items-center space-x-2 border-t pt-4">
             <Image
